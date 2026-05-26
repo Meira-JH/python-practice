ffb36693327202fd9dacc241595f89dc5a3c2032
Meira-JH <jhalvesmeira@gmail.com>
2026-05-26 14:59:36 -0300

    fear(02_01): parsing date

--- exercises/02-features/01-csv-reader/csv-reader.py
+++ exercises/02-features/01-csv-reader/csv-reader.py
@@ -1,5 +1,6 @@
 import csv
 from pprint import pprint
+from datetime import datetime
 
 EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
 
@@ -19,4 +20,7 @@ with open("laureates.csv", "r") as csvFile:
 for laureate in laureatesList:
     if laureate['surname'].lower() == "Einstein".lower():
         pprint(laureate)
+        year_date = datetime.strptime(laureate["year"], "%Y")
+        born_date = datetime.strptime(laureate["born"], "%Y-%m-%d")
+        print("age", year_date.year - born_date.year)
         break
